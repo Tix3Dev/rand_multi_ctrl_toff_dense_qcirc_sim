046dafedfaebe816a86a4fc55e584bdb3142369b
Tix3Dev <yves.vollmeier.code@gmail.com>
2024-12-10 16:28:14 +0100

    Clean up

--- circuit-runner/calling_circuit_runner_test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def run_command():
-    command = ["./target/release/main", "python_generated_dot.txt"]
-    result = subprocess.run(command, capture_output=True, text=True)
-    print(result.stdout)
-
-if __name__ == "__main__":
-    run_command()
